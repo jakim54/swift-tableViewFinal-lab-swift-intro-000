782bb62827ed6a5f28cf3e909ae303e8ad1de75e
jajuki13 <khkim1776@gmail.com>
2017-03-09 00:05:18 -0500

    started on some, instructions are still confusing, made me overthink

--- TableViewFinal/FactTableViewController.swift
+++ TableViewFinal/FactTableViewController.swift
@@ -9,4 +9,26 @@
 import UIKit
 
 class FactTableViewController: UITableViewController {
+    
+    var planet: Planet!
+    let factCellIdentifier = "factCell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = planet.name.capitalized
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return planet.facts.count
+    }
+    
+    override func tableView(_: UITableView, cellForRowAtIndexPath indexPath: indexPath) -> UITableViewCell {
+        // need to implement this
+        return UITableViewCell
+    }
+    
 }
--- TableViewFinal/PlanetTableViewController.swift
+++ TableViewFinal/PlanetTableViewController.swift
@@ -9,4 +9,21 @@
 import UIKit
 
 class PlanetTableViewController: UITableViewController {
+    
+    let planetCellIdentifier = "planetCell"
+    var planets: [Planet] = []
+    
+    func generatePlanetInfo() {
+        
+        let earthFacts = [
+            "Earth is the only planet not named after a god.",
+            "Earth has a powerful magnetic field.",
+            "The Earth was once believed to be the center of the universe."
+        ]
+        
+        let earth = Planet(name: "Earth", numberOfMoons: 1, fullOrbit: 365.26, facts: earthFacts)
+        planets.append(earth)
+        
+    }
+    
 }
